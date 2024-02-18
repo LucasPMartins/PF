5b2536e7e9cbad3a49111344137c7e68f060cdc1
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2024-02-18 07:15:06 -0300

    new

--- Aula04.hs
+++ Aula04.hs
@@ -1,3 +1,5 @@
+{-Assuntos: Recursão, Casamento de Padrões, Avaliação Preguiçosa-}
+
 {-1. O fatorial duplo de um número natural n é o produto de todos os números de 1 (ou 2)
 até n, contados de 2 em 2. Por exemplo, o fatorial duplo de 8 é 8 × 6 × 4 × 2 = 384, e
 o fatorial duplo de 7 é 7 × 5 × 3 × 1 = 105. Defina uma função para calcular o fatorial
